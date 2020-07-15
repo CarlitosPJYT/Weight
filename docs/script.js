7c956f4723e7ab5165d0adcd7d5e918e4e0bf5f3
Carlitos <juanes10ospina@hotmail.com>
2020-07-15 10:03:10 -0500

    push

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,8 @@
+var user = prompt("What is your weight?")
 var Tierra = 9.8
 var Marte = 3.7
 var Jupiter = 24.8
-var Weight = 50
+var Weight = parseInt(user)
 var result
 result = Weight * Marte / Tierra
 result = parseInt(result)
