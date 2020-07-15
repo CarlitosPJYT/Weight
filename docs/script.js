d93b304506aeb6f2cd6b71561e7e34cb30575884
Carlitos <juanes10ospina@hotmail.com>
2020-07-15 11:11:15 -0500

    init

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
 var user = prompt("What is your weight?")
+var planet = parseInt(prompt("To choose"))
 var Tierra = 9.8
 var Marte = 3.7
 var Jupiter = 24.8
@@ -7,4 +8,4 @@ var result
 
 result = Weight * Marte / Tierra
 result = parseInt(result)
-document.write(result)
+document.write("<p class='text-gray-300'" + result)
